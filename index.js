d559336318eba9c73a3b0a6ebbef7dc6a682aec9
Weyk1 <zefelradaiskarl@gmail.com>
2022-01-22 10:56:26 +0100

    partie1

---
+++ index.js
@@ -0,0 +1,53 @@
+// Exercice 1 :
+// let hello = "Hello world !"
+// alert (hello)
+
+
+//Exercice 2 :
+// let lastName = "Nom"
+// let firstName = "Prenom"
+// let city = "Ville"
+// alert ( "Nom : "+ firstName + "\nPrénom : "+ lastName + "\nVille : "+ city)
+
+
+//Exercice 3 :
+// let Name = prompt("Votre nom ?");
+// alert("Bonjour, " + Name);
+
+
+//Exercice 4 :
+// let lastName = prompt("Quel est votre nom ?");
+// let firstName = prompt("Quel est votre prénom ?");
+// let city = prompt("D'où venez vous ?");
+// alert ( "Bonjour "+ firstName+" " + lastName +" qui vient de "+ city)
+
+//Exercice 5 :
+// let Nombre1 = prompt("Quel est votre premier nombre à virgule ?");
+// let Nombre1Trunc = Math.trunc(Nombre1);
+// let Nombre2 = prompt("Quel est votre deuxieme nombre à virgule ?");
+// let calcul = (Nombre1Trunc * Nombre2)
+// alert ( "Le resultat est : " + calcul)
+
+//Exercice 6 :
+// let Nombre1 = prompt("Quel est votre premier nombre à virgule ?");
+// let Nombre2 = prompt("Quel est votre deuxieme nombre à virgule ?");
+// let calcul = (Nombre1 / Nombre2)
+// alert ( "Le resultat est : " + calcul)
+
+//Exercice 7 :
+// Soluce 1 :
+// let pointure = prompt("votre pointure")
+// let resultatX2 = (pointure * 2)
+// resultat5 = resultatX2 + 5
+// resultat50 = resultat5 * 50
+// resultat = parseInt(resultat50)
+
+// let annee_naissance = prompt("votre année de naissance")
+// annee_naissance = parseInt(annee_naissance)
+// reponse = resultat + annee_naissance
+// parseInt(reponse)
+// alert(reponse)
+
+// Soluce 2 :
+// let naissance = prompt ("quel est votre date de naissance") ;
+//alert ((((Number(pointure) 2)+5 )50) - (naissance) + 1766)
